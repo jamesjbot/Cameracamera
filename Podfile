ff531c7ea5cac865b1eac975729fb00555be0cd0
jamesjbot <jongs.j@gmail.com>
2017-08-21 00:18:02 -0400

    chore: Added files and folder to gitignore

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '10.3'
+use_frameworks!
+
+target 'Cameracamera' do 
+     pod 'Bond', '~> 6.0'
+end
+	
---
+++ Pods/Target Support Files/Pods-Cameracamera/Pods-Cameracamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cameracamera : NSObject
+@end
+@implementation PodsDummy_Pods_Cameracamera
+@end
