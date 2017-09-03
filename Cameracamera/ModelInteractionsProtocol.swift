a038e84cab0300a81a86cd34df1dfef3f4ac23e4
jamesjbot <jongs.j@gmail.com>
2017-09-03 01:52:23 -0400

    Add comments to ModelInteractions Protocol

--- Cameracamera/ModelInteractionsProtocol.swift
+++ Cameracamera/ModelInteractionsProtocol.swift
@@ -10,9 +10,12 @@ import Foundation
 import AVFoundation
 import Bond
 
+/// This protocol provides the ViewModel with access into the Model.
 protocol ModelInteractions: AVCapturePreviewProvider {
 
+    // This is update with metadata object detected in the last cycle
     var metadataCodeObjects: MutableObservableArray<MetaDataObjectAndPayload> { get }
 
+    // This function allows us to call down to the model to take a picture.
     func savePhoto(_ completion: ((Bool)->())?)
 }
